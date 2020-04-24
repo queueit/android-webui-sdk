90952d02acd1ad9ca65323474f3ac8192fda8c0d
MohammadMQ <38454801+MohammadMQ@users.noreply.github.com>
2020-04-24 10:39:45 +0200

    #6188 - Android SDK not returning queueittoken (in callback) for events in safetynet mode (#11)

--- library/src/main/java/com/queue_it/androidsdk/QueueServiceListener.java
+++ library/src/main/java/com/queue_it/androidsdk/QueueServiceListener.java
@@ -1,6 +1,6 @@
 package com.queue_it.androidsdk;
 
 public interface QueueServiceListener {
-    void onSuccess(String queueId, String queueUrlString, int queueUrlTtlInMinutes, String eventTargetUrl);
+    void onSuccess(String queueId, String queueUrlString, int queueUrlTtlInMinutes, String eventTargetUrl, String queueItToken);
     void onFailure(String errorMessage, int errorCode);
 }
