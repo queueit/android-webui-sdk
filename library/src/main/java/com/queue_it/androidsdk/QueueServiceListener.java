30a76894919ed65ad3d603e12f789ec045e10e19
Felix E. Drud <fedr@queue-it.com>
2025-11-13 09:56:30 +0100

    Sync GitHub repository, to remove files deleted from source repo

--- library/src/main/java/com/queue_it/androidsdk/QueueServiceListener.java
+++
@@ -1,6 +0,0 @@
-package com.queue_it.androidsdk;
-
-public interface QueueServiceListener {
-    void onSuccess(String queueId, String queueUrlString, int queueUrlTtlInMinutes, String eventTargetUrl, String queueItToken);
-    void onFailure(String errorMessage, int errorCode);
-}
